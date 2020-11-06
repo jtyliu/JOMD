198dc17037788ec82dace35e09755561c49390eb
JoshuaTianYangLiu <josh.liu@student.tdsb.on.ca>
2020-11-06 16:54:38 +0000

    Added reload function

---
+++ cogs/admin.py
@@ -0,0 +1,26 @@
+from discord.ext import commands
+from pathlib import Path
+
+
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def cog_check(self, ctx):
+        return await self.bot.is_owner(ctx.author)
+
+    @commands.command(hidden=True)
+    async def reload_all(self, ctx):
+        """Reload a module"""
+        try:
+            cogs = [file.stem for file in Path('cogs').glob('*.py')]
+            for extension in cogs:
+                self.bot.reload_extension(f'cogs.{extension}')
+        except commands.ExtensionError as e:
+            await ctx.send(f'{e.__class__.__name__}: {e}')
+        else:
+            await ctx.send('All cogs have been reloaded')
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
