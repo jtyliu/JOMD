ff9621121ed1aa9cd2282acab0bd8eba95855ed0
JoshuaTianYangLiu <josh.liu@student.tdsb.on.ca>
2021-01-27 12:20:55 -0500

    Added barebone +stats

--- cogs/meta.py
+++ cogs/meta.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from utils.query import Query
+from utils.db import session, Problem as Problem_DB
 
 
 class Meta(commands.Cog):
@@ -20,6 +21,28 @@ class Meta(commands.Cog):
         except:
             await ctx.send('Seems like I\'m getting cloud flared, rip')
 
+    @commands.command()
+    async def stats(self, ctx):
+        """Display cool dmoj stats that noone asked for"""
+        problems = session.query(Problem_DB.points)\
+            .order_by(Problem_DB.points.desc()).all()
+
+        def tuple_first(data):
+            return data[0]
+
+        def calculate_points(points, fully_solved):
+            b = 150*(1-0.997**fully_solved)
+            p = 0
+            for i in range(min(100, len(points))):
+                p += (0.95**i)*points[i]
+            return b+p
+
+        problems = list(map(tuple_first, problems))
+        total_problems = len(problems)
+        total_points = calculate_points(problems, total_problems)
+        await ctx.send("The theoretical maximum number of points "
+                       "you can achieve is %.2f" % total_points)
+        return
 
 def setup(bot):
     bot.add_cog(Meta(bot))
