444d1b921eb38081ad26ea891662cfa1a1752471
unknown <33037196+Aaerialys@users.noreply.github.com>
2021-05-20 12:00:10 -0400

    jump to first or last

--- utils/jomd_common.py
+++ utils/jomd_common.py
@@ -127,13 +127,17 @@ async def scroll_message(ctx,bot,message,content):
     NEXT="\u25B6"
     PREV2="\u23EA"
     NEXT2="\u23E9"
+    FIRST="\u23EE"
+    LAST="\23ED"
+    await message.add_reaction(FIRST)
     await message.add_reaction(PREV2)
     await message.add_reaction(PREV)
     await message.add_reaction(NEXT)
     await message.add_reaction(NEXT2)
+    await message.add_reaction(LAST)
     page=0
     def check(reaction, user): #from stackoverflow :monkey:
-        return not user.bot and str(reaction.emoji) in [PREV,NEXT,PREV2,NEXT2]
+        return not user.bot and str(reaction.emoji) in [PREV,NEXT,PREV2,NEXT2,FIRST,LAST]
     while True:
         try:
             reaction, user = await bot.wait_for("reaction_add", timeout=60, check=check)
@@ -146,6 +150,10 @@ async def scroll_message(ctx,bot,message,content):
                 page=max(0,min(len(content)-1,page-10))
             elif str(reaction.emoji) == NEXT2:
                 page=max(0,min(len(content)-1,page+10))
+            elif str(reaction.emoji) == FIRST:
+                page=0
+            elif str(reaction.emoji) == LAST:
+                page=len(content)-1
             await message.edit(content=content[page])
             await message.remove_reaction(reaction, user)
         except asyncio.TimeoutError:
@@ -156,10 +164,14 @@ async def scroll_embed(ctx,bot,message,title,content):
     NEXT="\u25B6"
     PREV2="\u23EA"
     NEXT2="\u23E9"
+    FIRST="\u23EE"
+    LAST="\23ED"
+    await message.add_reaction(FIRST)
     await message.add_reaction(PREV2)
     await message.add_reaction(PREV)
     await message.add_reaction(NEXT)
     await message.add_reaction(NEXT2)
+    await message.add_reaction(LAST)
     page=0
     def check(reaction, user): #from stackoverflow :monkey:
         return not user.bot and str(reaction.emoji) in [PREV,NEXT,PREV2,NEXT2]
@@ -175,6 +187,10 @@ async def scroll_embed(ctx,bot,message,title,content):
                 page=max(0,min(len(content)-1,page-10))
             elif str(reaction.emoji) == NEXT2:
                 page=max(0,min(len(content)-1,page+10))
+            elif str(reaction.emoji) == FIRST:
+                page=0
+            elif str(reaction.emoji) == LAST:
+                page=len(content)-1
             await message.edit(embed=discord.Embed().add_field(name=title,value=content[page]))
             await message.remove_reaction(reaction, user)
         except asyncio.TimeoutError:
