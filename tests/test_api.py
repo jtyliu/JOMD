7ac3ca7de3ff3f9569271cb36c8979b041c7794c
JoshuaTianYangLiu <josh.liu@student.tdsb.on.ca>
2021-06-21 00:30:06 -0400

    Add tests for User Api model

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,8 @@
-import unittest
-from utils.api import API, ObjectNotFound
+from sqlalchemy.util.langhelpers import NoneType
+from utils.api import *
 import asyncio
+import json
+import pytest
 # Shrug
 # https://stackoverflow.com/questions/23033939/how-to-test-python-3-4-asyncio-code
 
@@ -14,9 +16,9 @@ def async_test(f):
     return wrapper
 
 
-class APITest(unittest.TestCase):
+class TestAPI:
     # Test the intended functionality of API.url_encode()
-    def setUp(self):
+    def setup_class(self):
         self.api = API()
 
     def test_url_encode_str(self):
@@ -25,10 +27,7 @@ class APITest(unittest.TestCase):
             "bbb": "ccc",
             1: "ddd"
         }
-        self.assertEqual(
-            self.api.url_encode(params),
-            "?aaa=bbb&bbb=ccc&1=ddd"
-        )
+        assert self.api.url_encode(params) == "?aaa=bbb&bbb=ccc&1=ddd"
 
     def test_url_encode_list(self):
         params = {
@@ -36,10 +35,7 @@ class APITest(unittest.TestCase):
             "eee": [1, 2, 3],
             1: ["fff", 5, 7]
         }
-        self.assertEqual(
-            self.api.url_encode(params),
-            "?aaa=bbb&aaa=ccc&aaa=ddd&eee=1&eee=2&eee=3&1=fff&1=5&1=7"
-        )
+        assert self.api.url_encode(params) == "?aaa=bbb&aaa=ccc&aaa=ddd&eee=1&eee=2&eee=3&1=fff&1=5&1=7"
 
     def test_url_encode_list_str(self):
         params = {
@@ -47,44 +43,33 @@ class APITest(unittest.TestCase):
             "bbb": "ccc",
             1: ["eee", 123, "fff"]
         }
-        self.assertEqual(
-            self.api.url_encode(params),
-            "?aaa=bbb&bbb=ccc&1=eee&1=123&1=fff"
-        )
+        assert self.api.url_encode(params) == "?aaa=bbb&bbb=ccc&1=eee&1=123&1=fff"
 
     def test_url_encode_escape_chr(self):
         params = {
             "user": "JoshuaL&page=1",
             "page": 1
         }
-        self.assertEqual(
-            self.api.url_encode(params),
-            "?user=JoshuaL%26page%3D1&page=1"
-        )
+        assert self.api.url_encode(params) == "?user=JoshuaL%26page%3D1&page=1"
+
         params = {
             "user": "JoshuaL#",
             "page": 1
         }
-        self.assertEqual(
-            self.api.url_encode(params),
-            "?user=JoshuaL%23&page=1"
-        )
+        assert self.api.url_encode(params) == "?user=JoshuaL%23&page=1"
 
     def test_url_encode_none(self):
         params = {
             "user": "JoshuaL",
             "page": None
         }
-        self.assertEqual(
-            self.api.url_encode(params),
-            "?user=JoshuaL"
-        )
+        assert self.api.url_encode(params) == "?user=JoshuaL"
 
     class DataMock:
         async def parse(self, data, _type):
             return data
 
-    @async_test
+    @pytest.mark.asyncio
     async def test_parse_data(self):
         api_data = self.api.Data
         self.api.Data = self.DataMock
@@ -98,13 +83,12 @@ class APITest(unittest.TestCase):
             }
         }
         await self.api.parse(data, object)
-        self.assertEqual(self.api.api_version, data["api_version"])
-        self.assertEqual(self.api.method, data["method"])
-        self.assertEqual(self.api.fetched, data["fetched"])
-        self.assertEqual(self.api.data, data["data"])
-        self.api.Data = api_data
+        assert self.api.api_version == data["api_version"]
+        assert self.api.method == data["method"]
+        assert self.api.fetched == data["fetched"]
+        assert self.api.data == data["data"]
 
-    @async_test
+    @pytest.mark.asyncio
     async def test_parse_error(self):
         self.api.Data = self.DataMock
         data = {
@@ -116,9 +100,57 @@ class APITest(unittest.TestCase):
                 "message": "User not found"
             }
         }
-        with self.assertRaises(ObjectNotFound):
+        with pytest.raises(ObjectNotFound):
             await self.api.parse(data, object)
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestAPIModels:
+
+    def open_file(self, filename):
+        with open('./data/' + filename, 'r') as f:
+            return f.read()
+
+    def open_json(self, filename):
+        return json.loads(self.open_file(filename))
+
+    class Jsonn:
+        @classmethod
+        def from_dict(self, j):
+            obj = self()
+            obj.__dict__.update(j)
+            return obj
+
+    def test_user_model(self):
+        # user = json.loads(self.open_file('user.json'), object_hook=self.Jsonn.from_dict)
+        user = User(self.open_json('user.json'))
+        assert user.id == 27823
+        assert user.username == 'JoshuaL'
+        assert type(user.points) is float
+        assert type(user.performance_points) is float
+        assert type(user.problem_count) is int
+        assert all(type(code) is str for code in user.solved_problems)
+        assert user.rank == 'user'
+        assert type(user.rating) is int
+        assert type(user.volatility) is int
+        assert all(type(org) is int for org in user.organizations)
+
+        for contest in user.contests:
+            assert type(contest.key) is str
+            assert type(contest.score) is float
+            assert type(contest.cumulative_time) is int
+            assert isinstance(contest.rating, (int, NoneType))
+            assert isinstance(contest.volatility, (int, NoneType))
+
+        if hasattr(user, '_contests'):
+            for contest in user._contests:
+                assert type(contest['key']) is str
+                assert type(contest['score']) is float
+                assert type(contest['cumulative_time']) is int
+                assert isinstance(contest['rating'], (int, NoneType))
+                assert isinstance(contest['volatility'], (int, NoneType))
+
+        if hasattr(user, '_solved_problems'):
+            assert all(type(code) is str for code in user._solved_problems)
+
+        if hasattr(user, '_organizations'):
+            assert all(type(org) is int for org in user._organizations)
