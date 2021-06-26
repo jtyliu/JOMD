59158afa38f71cdb30dc3202038b0076674738d7
JoshuaTianYangLiu <josh.liu@student.tdsb.on.ca>
2021-06-26 15:02:19 -0400

    Escape markdown

--- cogs/handles.py
+++ cogs/handles.py
@@ -2,6 +2,7 @@ from utils.api import ObjectNotFound
 from utils.jomd_common import scroll_embed
 from operator import itemgetter
 from discord.ext import commands
+from discord.utils import escape_markdown
 import discord
 from utils.query import Query
 from utils.db import session, User as User_DB, Handle as Handle_DB, Contest as Contest_DB
@@ -18,48 +19,50 @@ class Handles(commands.Cog):
     @commands.command()
     async def whois(self, ctx, member: typing.Optional[discord.Member] = None,
                     handle: typing.Optional[str] = None):
-
-        query = Query()
-        username, linked_username, pfp = None, None, None
-        if handle:
-            user = None
-            try:
-                user = await query.get_user(handle)
-            except ObjectNotFound:
-                username = None
-            if user:
-                handle = user.username
-                author_id = query.get_handle_user(handle, ctx.guild.id)
-                username = handle
-                if author_id:
-                    member = ctx.message.guild.get_member(author_id)
-                    linked_username = member.nick or member.name
-                    pfp = member.avatar_url
-        elif member:
-            handle = query.get_handle(member.id, ctx.guild.id)
-            username = member.nick or member.name
+        try:
+            query = Query()
+            username, linked_username, pfp = None, None, None
             if handle:
-                linked_username = handle
-                pfp = await query.get_pfp(handle)
-        if linked_username:
-            embed = discord.Embed(
-                color=0xfcdb05,
-                title=f'{username} is {linked_username}',
-            )
-            embed.set_thumbnail(url=pfp)
-            await ctx.send(embed=embed)
-        elif username:
-            embed = discord.Embed(
-                title=f'{username} is not linked with any account here',
-                color=0xfcdb05,
-            )
-            await ctx.send(embed=embed)
-        else:
+                user = None
+                try:
+                    user = await query.get_user(handle)
+                except ObjectNotFound:
+                    username = None
+                if user:
+                    handle = user.username
+                    author_id = query.get_handle_user(handle, ctx.guild.id)
+                    username = handle
+                    if author_id:
+                        member = ctx.message.guild.get_member(author_id)
+                        linked_username = member.nick or member.name
+                        pfp = member.avatar_url
+            elif member:
+                handle = query.get_handle(member.id, ctx.guild.id)
+                username = member.nick or member.name
+                if handle:
+                    linked_username = handle
+                    pfp = await query.get_pfp(handle)
+            if linked_username:
+                embed = discord.Embed(
+                    color=0xfcdb05,
+                    title=escape_markdown(f'{username} is {linked_username}'),
+                )
+                embed.set_thumbnail(url=pfp)
+                return await ctx.send(embed=embed)
+            elif username:
+                embed = discord.Embed(
+                    title=escape_markdown(f'{username} is not linked with any account here'),
+                    color=0xfcdb05,
+                )
+                return await ctx.send(embed=embed)
+        except Exception:
+            pass
+        finally:
             name = None
             if member:
                 name = member.nick or member.name
             embed = discord.Embed(
-                title=f'Nothing found on {handle or name}',
+                title=escape_markdown(f'Nothing found on {handle or name}'),
                 color=0xfcdb05,
             )
             await ctx.send(embed=embed)
@@ -67,6 +70,7 @@ class Handles(commands.Cog):
     @commands.command()
     async def unlink(self, ctx):
         '''Unlink your discord account with your dmoj account'''
+        # TODO: Add admin ability to manually unlink
         query = Query()
         if not query.get_handle(ctx.author.id, ctx.guild.id):
             await ctx.send('You are not linked with any user')
@@ -76,7 +80,7 @@ class Handles(commands.Cog):
             .filter(Handle_DB.guild_id == ctx.guild.id).first()
         session.delete(handle)
         session.commit()
-        await ctx.send(f'Unlinked you with handle {handle.handle}')
+        await ctx.send(escape_markdown(f'Unlinked you with handle {handle.handle}'))
 
     @commands.command(usage='dmoj_handle')
     async def link(self, ctx, username: str):
@@ -86,7 +90,7 @@ class Handles(commands.Cog):
         user = await query.get_user(username)
 
         if user is None:
-            await ctx.send(f'{username} does not exist on DMOJ')
+            await ctx.send(escape_markdown(f'{username} does not exist on DMOJ'))
             return
 
         username = user.username
@@ -118,17 +122,17 @@ class Handles(commands.Cog):
         handle.guild_id = ctx.guild.id
         session.add(handle)
         session.commit()
-        await ctx.send(
+        await ctx.send(escape_markdown(
             '%s, you now have linked your account to %s' %
             (ctx.author.name, username)
-        )
+        ))
 
         rank_to_role = {role.name: role for role in ctx.guild.roles if role.name in RANKS}
         rank = self.rating_to_rank(user.rating)
         if rank in rank_to_role:
             await self._update_rank(ctx.author, rank_to_role[rank], 'Dmoj account linked')
         else:
-            await ctx.send('You are missing the ' + rank.name + ' role')
+            await ctx.send('You are missing the `' + rank.name + '` role')
 
     @commands.command(name='set', usage='discord_account [dmoj_handle, +remove]')
     @commands.has_any_role(*ADMIN_ROLES)
@@ -141,14 +145,14 @@ class Handles(commands.Cog):
             user = await query.get_user(username)
 
             if user is None:
-                await ctx.send(f'{username} does not exist on dmoj')
+                await ctx.send(escape_markdown(f'{username} does not exist on dmoj'))
                 return
 
             username = user.username
 
         handle = query.get_handle(member.id, ctx.guild.id)
         if handle == username:
-            return await ctx.send(f'{member.display_name} is already linked with {handle}')
+            return await ctx.send(escape_markdown(f'{member.display_name} is already linked with {handle}'))
 
         if handle:
             handle = session.query(Handle_DB)\
@@ -156,7 +160,7 @@ class Handles(commands.Cog):
                 .filter(Handle_DB.guild_id == ctx.guild.id).first()
             session.delete(handle)
             session.commit()
-            await ctx.send(f'Unlinked {member.display_name} with handle {handle.handle}')
+            await ctx.send(escape_markdown(f'Unlinked {member.display_name} with handle {handle.handle}'))
 
         if username == '+remove':
             return
@@ -172,14 +176,14 @@ class Handles(commands.Cog):
         handle.guild_id = ctx.guild.id
         session.add(handle)
         session.commit()
-        await ctx.send(f'Linked {member.name} with {username}')
+        await ctx.send(escape_markdown(f'Linked {member.name} with {username}'))
 
         rank_to_role = {role.name: role for role in ctx.guild.roles if role.name in RANKS}
         rank = self.rating_to_rank(user.rating)
         if rank in rank_to_role:
             await self._update_rank(ctx.author, rank_to_role[rank], 'Dmoj account linked')
         else:
-            await ctx.send('You are missing the ' + rank.name + ' role')
+            await ctx.send('You are missing the `' + rank.name + '` role')
 
     @commands.command(aliases=['users', 'leaderboard'], usage='[rating|maxrating|points|solved]')
     async def top(self, ctx, arg='rating'):
